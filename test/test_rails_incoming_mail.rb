668f2f2965b565984ceca3013dd4120a172d06ed
Willem van der Jagt <willem.vanderjagt@shopify.com>
2015-01-11 21:44:23 -0500

    server test WIP

--- test/test_rails_incoming_mail.rb
+++ test/test_rails_incoming_mail.rb
@@ -1,5 +1,13 @@
 require 'minitest/autorun'
 require 'rails_incoming_mail'
 
-class RailsIncomingMailTest < Minitest::Unit::TestCase
+class RailsIncomingMailServerTest < Minitest::Test
+
+  def setup
+    @s = RailsIncomingMail::Server.new("the.domain")
+  end
+
+  def test_serve
+
+  end
 end
